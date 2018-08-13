f6600e314e5c9a0c3bb636be7458699ff418a921
Natacha De la Rosa <delarosa.natacha@gmail.com>
2018-08-13 00:06:55 -0400

    added  store details skeleton and product skeleton.

---
+++ src/components/product/ProductList.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitting: false
+    };
+  }
+  render() {
+    const { title, list } = this.props;
+    return (
+      <div>
+        <h3>{title}</h3>
+        <div className="row">
+          {list.map((product, i) => (
+            <Product key={product.id} {...product} />
+          ))}
+          {list.length === 0 ? <p>No products available</p> : ""}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
--- src/components/store/StoreDetails.js
+++ src/components/store/StoreDetails.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import ProductList from "../product/ProductList";
+import Jumbotron from "../../components/site/Jumbotron";
 
 class StoreDetails extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      products: []
+    };
   }
 
   componentDidMount() {
@@ -11,15 +15,45 @@ class StoreDetails extends Component {
       match: { params }
     } = this.props;
 
-    //TODO: Get store details from our contract and displayit here!
+    //TODO: Get store details from our contract and display it here!
     // Also get the list of products of this store
     // Store id comes from ${params.storeId}
+
+    //loading sample products
+    this.loadProducts();
   }
 
+  loadProducts = async () => {
+    // ============== BEGIN: Function implementation here ================ //
+
+    // TODO: We want to load the lists of stores that our contract has
+    // this.state.stores expects an array of objects with these attributes:
+    // {
+    //   id: "store Id",
+    //   idOwner: "owner address or id",
+    //   title: "token attribute",
+    //   description
+    //   metadataHash: "store attributes",
+    // }
+
+    // Example:
+    const product = {
+      id: 1,
+      title: "My Product",
+      description: "My sample product in the marketplace",
+      price: 3,
+      metadataHash: "METADATA"
+    };
+    const list = [product];
+    this.setState({ products: list });
+  };
+
   render() {
+    const { products } = this.state;
     return (
       <div>
-        <h2>Store Details</h2>
+        <Jumbotron title="Store Name" subtitle="Store Description!" />
+        <ProductList title="Products" list={products} />
       </div>
     );
   }
