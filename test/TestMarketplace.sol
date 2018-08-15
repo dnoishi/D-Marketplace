2ae0c0acc5701e8cfab89e7687789a4fcec88fb8
Natacha De la Rosa <delarosa.natacha@gmail.com>
2018-08-15 00:47:29 -0400

    changes

--- test/TestMarketplace.sol
+++ test/TestMarketplace.sol
@@ -9,10 +9,4 @@ contract TestMarketplace {
         Marketplace marketplace = new Marketplace();
         Assert.equal(marketplace.owner(), this, "The owner is different than the deployer");
     }
-
-    function testOwnerCanAddAdmin() public{
-        Marketplace marketplace = new Marketplace();
-        marketplace.registerAdmin(this);
-        Assert.equal(marketplace.AdminAddresses(1), this, "Admin address distinct that the one received");
-    }
 }
